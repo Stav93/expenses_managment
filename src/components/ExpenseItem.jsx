a0c1dedff4a5d0b641653f617617a07bf5fded83
Stav <stavush93@gmail.com>
2022-08-19 18:19:03 +0300

    spliting date to readble

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,12 +1,17 @@
 import "./ExpenseItem.css"
 
 function ExpenseItem({title, date, amount}) {
-  // const expenseDate = new Date(2020, 2, 28);
-  // const expenseTitle = "Car Insurance";
-  // const expenseAmount = 247.67
+  const month = date.toLocaleString("en-US", { month: "long" });
+  const year = date.toLocaleString("en-US", { year: "2-digit" })
+  const day = date.getFullYear();
+
   return (
     <div className="expense-item">
-      <div>{date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">{amount}</div>
