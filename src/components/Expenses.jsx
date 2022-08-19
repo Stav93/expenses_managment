1e94dee54f9f1f32c44bde4f2283d2a26aa704c3
Stav <stavush93@gmail.com>
2022-08-19 19:31:33 +0300

    adding Card (UI) component

--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -1,31 +1,32 @@
-import "./Expenses.css"
-import ExpenseItem from "./ExpenseItem"
+import "./Expenses.css";
+import ExpenseItem from "./ExpenseItem";
+import Card from "./UI/Card"
 
-function Expenses({expenses}) {
+function Expenses({ expenses }) {
   return (
-    <div className="expenses">
+      <Card className="expenses">
         <ExpenseItem
-        title={expenses[0].title}
-        date={expenses[0].date}
-        amount={expenses[0].amount}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        date={expenses[1].date}
-        amount={expenses[1].amount}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        date={expenses[2].date}
-        amount={expenses[2].amount}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        date={expenses[3].date}
-        amount={expenses[3].amount}
-      />
-    </div>
-  )
+          title={expenses[0].title}
+          date={expenses[0].date}
+          amount={expenses[0].amount}
+        />
+        <ExpenseItem
+          title={expenses[1].title}
+          date={expenses[1].date}
+          amount={expenses[1].amount}
+        />
+        <ExpenseItem
+          title={expenses[2].title}
+          date={expenses[2].date}
+          amount={expenses[2].amount}
+        />
+        <ExpenseItem
+          title={expenses[3].title}
+          date={expenses[3].date}
+          amount={expenses[3].amount}
+        />
+      </Card>
+  );
 }
 
-export default Expenses
+export default Expenses;
