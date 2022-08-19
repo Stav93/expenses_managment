0b20d08f639e9a15ddfbfe409c317fba15df704c
Stav <stavush93@gmail.com>
2022-08-19 18:06:43 +0300

    passing props

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,12 +1,15 @@
 import "./ExpenseItem.css"
 
-function ExpenseItem() {
+function ExpenseItem({title, date, amount}) {
+  // const expenseDate = new Date(2020, 2, 28);
+  // const expenseTitle = "Car Insurance";
+  // const expenseAmount = 247.67
   return (
     <div className="expense-item">
-      <div>Date</div>
+      <div>{date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Title</h2>
-        <div className="expense-item__price">Amount</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">{amount}</div>
       </div>
    </div>
   );
