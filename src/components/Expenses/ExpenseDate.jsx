ebaa33569d16bb355cbdfa805ca93aba5c1c1fb3
Stav <stavush93@gmail.com>
2022-08-21 21:04:21 +0300

    adding a New Expense Form

--- src/components/Expenses/ExpenseDate.jsx
+++ src/components/Expenses/ExpenseDate.jsx
@@ -1,4 +1,4 @@
-import "./ExpenseDate.css"
+import './ExpenseDate.css';
 
 function ExpenseDate({date}) {
   const month = date.toLocaleString("en-US", { month: "long" });
---
+++ src/components/NewExpense/NewExpense.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+import ExpenseForm from "./ExpenseForm"
+import "./NewExpense.css"
+
+function NewExpense() {
+
+  return (
+    <div className="new-expense">
+      <ExpenseForm/>
+    </div>
+  )
+}
+
+export default NewExpense
